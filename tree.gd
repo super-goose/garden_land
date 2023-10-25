03c549d36109d0f192ba940643aa8bb4dc1589e4
Tex <james.davis@zenlist.com>
2023-10-25 16:09:31 -0500

    clear a stump after the tree is chopped down

--- tree.gd
+++ tree.gd
@@ -1,17 +1,19 @@
 class_name FruitTree
 extends Area2D
 
-var hp = 10
+var hp = 2
 var is_intact = true
 
 func get_chopped():
 	print('tree is getting chopped')
-	if not is_intact:
-		return
-	hp -= 4
-	$FullTree.play("wind")
-	if hp < 0:
-		is_intact = false
-		$FullTree.visible = false
-		$FullTree/StaticBody2D.collision_layer = 0
-		collision_layer = 0
+	hp -= 1
+	if is_intact:
+		$FullTree.play("wind")
+		if hp < 0:
+			is_intact = false
+			$FullTree.visible = false
+			$FullTree/StaticBody2D.collision_layer = 0
+#			collision_layer = 0
+	else:
+		if hp == -2:
+			queue_free()
